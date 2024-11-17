22c8e735a30f05b2dc411efb233d4ca2c59acaa4
Sam9427 <Sam9427@nyu.edu>
2024-11-17 18:23:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let color = "pink"
+function colorwheel(color){
+	if (color == "pink"){
+		console.log("yess" + color + "is a pretty color, great choice!");
+	}
+	else if (color == "blue"){
+		console.log("wow you have great taste, the color blue is so gorgeous!");
+	}
+	else{
+		console.log("pookie I apologize but I dont think your colors match up");
+	}
+}
